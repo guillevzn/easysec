eeb2d1cb252efc4518971615e12d42f0a76355af
Guillermo Vizcaíno Román <guillevzn@gmail.com>
2023-07-19 16:28:15 +0100

    Summary

--- setup.py
+++ setup.py
@@ -7,18 +7,18 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'Python library to import data from SEC.gov.'
 
 # Setting up
 setup(
-    name="easyfred",
+    name="easysec",
     version=VERSION,
     author="Guillermo Vizcaíno Román",
     author_email="<guillevzn@gmail.com>",
     description=DESCRIPTION,
     packages=find_packages(),
-    install_requires=['pandas', 'requests', 'json'],
+    install_requires=['pandas'],
     keywords=['python', 'sec', 'database', 'API' ],
     classifiers=[
         "Development Status :: 1 - Planning",
